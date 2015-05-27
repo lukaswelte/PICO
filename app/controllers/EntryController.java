dcecd8aa3a9c98f0dc322d67743cb29ab1c4a163
Thomas Wassmer <wassmer.thomas@googlemail.com>
2015-05-27 16:35:40 +0200

    delete class create entry import method createentry

--- app/controllers/EntryController.java
+++ app/controllers/EntryController.java
@@ -1,13 +1,94 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import models.Entry;
+import play.libs.Json;
 import play.mvc.Result;
 
+import javax.persistence.OptimisticLockException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 
 
 public class EntryController extends BaseController {
 
+
+    /**
+     * Create an entry with following inputs
+     * required input: Json containing the URL and the Title
+     * Example:
+     * (
+     *  "url": "http://test.de"
+     *  "title": "test"
+     *  )
+     * return API Response entry
+     */
+    public static Result createEntry() {
+        JsonNode json = request().body().asJson();
+        if (json == null) {
+            return invalidAPIInput();
+        }
+
+        String url = json.findPath("url").textValue();
+        Entry entry = new Entry();
+        if (url == null || !isValidURL(url)) {
+            return invalidAPIInput();
+        }
+
+        entry.url = url;
+
+        String title = json.findPath("title").textValue();
+        if (title == null) {
+            return invalidAPIInput();
+        } else {
+            entry.title = title;
+        }
+        String context = json.findPath("context").textValue();
+        if (context != null) {
+            entry.context = context;
+        }
+
+        try {
+            byte[] previewImage = json.findPath("previewImage").binaryValue();
+            if (previewImage != null) {
+                entry.previewImage = previewImage;
+            }
+        } catch (IOException e) {
+            //Ignore
+        }
+        try {
+        entry.save();
+        }catch (OptimisticLockException e){
+            // 
+        }
+        return jsonAPIResponse(Json.toJson(entry));
+    }
+
+
+    /**
+     * Validate the input if it is an correct URL; e.g: "www.coolors.io"
+     *
+     * @param pUrl The retrieved Object
+     * @return true if valid URL or false is not valid URL
+     */
+    private static boolean isValidURL(String pUrl) {
+
+        URL u = null;
+        try {
+            u = new URL(pUrl);
+        } catch (MalformedURLException e) {
+            return false;
+        }
+        try {
+            u.toURI();
+        } catch (URISyntaxException e) {
+            return false;
+        }
+        return true;
+    }
     /**
      * Retrieves a entry based on its ID
      *
@@ -41,20 +122,20 @@ public class EntryController extends BaseController {
      * @return API Response with List of entries
      * Example:
      * {
-     *      "status":200,
-     *      "data":[
-     *          {
-     *          "id":5,
-     *          "url":"www.test.de",
-     *          "title":"Erste Test Entry",
-     *          "context":"Das ist ein Context",
-     *          "thumbnailURL":null,
-     *          "registrationDate":1432157356000,
-     *          "lastUpdated":1432157356000,
-     *          "user":1,
-     *          "labels":[{"id":6,"name":"fooba","user":null,"entries":[5]}]
-     *          }
-     *      ]
+     * "status":200,
+     * "data":[
+     * {
+     * "id":5,
+     * "url":"www.test.de",
+     * "title":"Erste Test Entry",
+     * "context":"Das ist ein Context",
+     * "thumbnailURL":null,
+     * "registrationDate":1432157356000,
+     * "lastUpdated":1432157356000,
+     * "user":1,
+     * "labels":[{"id":6,"name":"fooba","user":null,"entries":[5]}]
+     * }
+     * ]
      * }
      */
     public static Result allEntries() {
