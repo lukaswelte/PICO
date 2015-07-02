4edfe95f7959c32bc77a220d423b3b174286379a
Belinda Zahra <zahra@in.tum.de>
2015-07-03 01:29:58 +0200

    #48 tried to write a evolution

---
+++ conf/evolutions/default/5.sql
@@ -0,0 +1,34 @@
+# --- !Ups
+SET FOREIGN_KEY_CHECKS=0;
+
+alter table entry drop constraint fk_entry_user_1;
+alter table label drop constraint fk_label_user_2;
+
+
+alter table entry_label drop constraint fk_entry_label_entry_01;
+alter table entry_label drop constraint fk_entry_label_label_02;
+
+alter table entry add constraint fk_entry_user_1 foreign key (user_id) references user (id) on delete cascade on update restrict;
+alter table label add constraint fk_label_user_2 foreign key (user_id) references user (id) on delete cascade on update restrict;
+
+alter table entry_label add constraint fk_entry_label_entry_01 foreign key (entry_id) references entry (id) on delete cascade on update restrict;
+alter table entry_label add constraint fk_entry_label_label_02 foreign key (label_id) references label (id) on delete cascade on update restrict;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+# --- !Downs
+SET FOREIGN_KEY_CHECKS=0;
+
+alter table entry drop constraint fk_entry_user_1;
+alter table label drop constraint fk_label_user_2;
+
+alter table entry_label drop constraint fk_entry_label_entry_01;
+alter table entry_label drop constraint fk_entry_label_label_02;
+
+alter table entry add constraint fk_entry_user_1 foreign key (user_id) references user (id) on delete restrict on update restrict;
+alter table label add constraint fk_label_user_2 foreign key (user_id) references user (id) on delete restrict on update restrict;
+
+alter table entry_label add constraint fk_entry_label_entry_01 foreign key (entry_id) references entry (id) on delete restrict on update restrict;
+alter table entry_label add constraint fk_entry_label_label_02 foreign key (label_id) references label (id) on delete restrict on update restrict;
+
+SET FOREIGN_KEY_CHECKS=1;
